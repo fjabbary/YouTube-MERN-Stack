baf282c084c44bb2855b620227029e8e11af02a3
unknown <farzin.jabbary@gmail.com>
2020-12-24 18:14:58 -0500

    express and initial api build

---
+++ backend/index.js
@@ -0,0 +1,33 @@
+const express = require('express'),
+    bodyParser = require('body-parser'),
+    app = express(),
+    PORT = 8080;
+
+const videoList = require('./data/videoList.json')
+const videoDetails = require('./data/videoDetails')
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+    res.json('app root')
+})
+
+app.get('/videos', (req, res) => {
+    res.json(videoList)
+})
+
+app.get('/videos/:id', (req, res) => {
+    const id = req.params.id;
+
+    const foundVideo = videoDetails.find(video => video.id == id)
+    res.json(foundVideo)
+})
+
+app.post('/video/add', (req, res) => {
+    res.json('add video')
+})
+
+app.listen(PORT, () => {
+    console.log(`App is running at ports ${PORT}`)
+})
