4c81c15f1a6405df8df030e86defa1a539febd1c
unknown <farzin.jabbary@gmail.com>
2020-12-26 19:57:20 -0500

    react router added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,22 @@
 
 import './App.css';
 import Main from './components/Main';
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+import Upload from './components/Upload';
 
 
 function App() {
   return (
-    <div className="App">
-      <Main />
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/" exact component={Main} />
+          <Route path="/upload" component={Upload} />
+        </Switch>
+      </div>
+    </Router>
+
+
   );
 }
 
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
-import './styles/Header.scss'
+import './styles/Header.scss';
+import { Link } from 'react-router-dom'
 
 class Header extends Component {
     render() {
         return (
             <div className="header">
                 <div className="header__left">
-                    <img src="./assets/Logo/Logo-brainflix.png" alt="logo" />
+                    <Link to="/"><img src="./assets/Logo/Logo-brainflix.png" alt="logo" /></Link>
                 </div>
                 <div className="header__right">
                     <form className="header__right--form">
                         <input type="text" className="header__right--input" placeholder="Search..." />
                         <button className="header__right--button"><i className="fa fa-search header__right--search"></i></button>
                     </form>
-                    <button className="header__right--upload">+ Upload</button>
+                    <Link to="/upload"><button className="header__right--upload">+ Upload</button></Link>
                 </div>
             </div>
         );
